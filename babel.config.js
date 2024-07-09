8498f3f9661431c0833243fe0b33552d6e945529
Anatoly Batov <play.hoffa@gmail.com>
2024-07-10 00:50:53 +0400

    commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
-const presets = [['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]];
+const presets = {
+    [['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]],
+    "env": {
+        "test": {
+            "presets": [
+                [
+                    "@babel/preset-env",
+                    {
+                        "targets": {
+                            "node": "current"
+                        }
+                    }
+                ]
+            ]
+        }
+    }
+};
 
 module.exports = { presets };
