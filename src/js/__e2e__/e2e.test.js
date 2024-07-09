8bee82d6565187f598852478de821c83393c4efa
Anatoly Batov <play.hoffa@gmail.com>
2024-07-10 01:09:44 +0400

    commit

--- src/js/__e2e__/e2e.test.js
+++ src/js/__e2e__/e2e.test.js
@@ -1,6 +1,6 @@
 import puppetteer from 'puppeteer';
-// import { CardValidatorWidget } from '../widget';
-// import { fork } from 'child_process';
+import { CardValidatorWidget } from '../widget';
+import { fork } from 'child_process';
 
 // jest.setTimeout(30000); // default puppeteer timeout
 
@@ -24,9 +24,9 @@ describe('Credit Card Validator form', () => {
     });
 
     browser = await puppetteer.launch({
-      // headless: false, // show gui
-      // slowMo: 250,
-      // devtools: true, // show devTools
+      headless: false, // show gui
+      slowMo: 250,
+      devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
@@ -44,33 +44,33 @@ describe('Credit Card Validator form', () => {
     });
 
     // eslint-disable-next-line jest/expect-expect
-    // test('Must add class valid if the number is valid', async () => {
-    //   await page.goto(baseUrl);
-    //   await page.waitFor(CardValidatorWidget.selector);
+    test('Must add class valid if the number is valid', async () => {
+      await page.goto(baseUrl);
+      await page.waitFor(CardValidatorWidget.selector);
 
-    //   const form = await page.$(CardValidatorWidget.formSelector);
-    //   const input = await form.$(CardValidatorWidget.inputSelector);
-    //   const submitter = await form.$(CardValidatorWidget.submitterSelector);
+      const form = await page.$(CardValidatorWidget.formSelector);
+      const input = await form.$(CardValidatorWidget.inputSelector);
+      const submitter = await form.$(CardValidatorWidget.submitterSelector);
 
-    //   await input.type('4539499701100246');
-    //   submitter.click();
+      await input.type('4539499701100246');
+      submitter.click();
 
-    //   await page.waitForSelector(CardValidatorWidget.inputSelector + '.valid');
-    // });
+      await page.waitForSelector(CardValidatorWidget.inputSelector + '.valid');
+    });
 
     // eslint-disable-next-line jest/expect-expect
-    // test('Must add class invalid if the number is not valid', async () => {
-    //   await page.goto(baseUrl);
-    //   await page.waitFor(CardValidatorWidget.selector);
+    test('Must add class invalid if the number is not valid', async () => {
+      await page.goto(baseUrl);
+      await page.waitFor(CardValidatorWidget.selector);
 
-    //   const form = await page.$(CardValidatorWidget.formSelector);
-    //   const input = await form.$(CardValidatorWidget.inputSelector);
-    //   const submitter = await form.$(CardValidatorWidget.submitterSelector);
+      const form = await page.$(CardValidatorWidget.formSelector);
+      const input = await form.$(CardValidatorWidget.inputSelector);
+      const submitter = await form.$(CardValidatorWidget.submitterSelector);
 
-    //   await input.type('4539499701100247');
-    //   submitter.click();
+      await input.type('4539499701100247');
+      submitter.click();
 
-    //   await page.waitForSelector(CardValidatorWidget.inputSelector + '.invalid');
-    // });
+      await page.waitForSelector(CardValidatorWidget.inputSelector + '.invalid');
+    });
   });
 });
